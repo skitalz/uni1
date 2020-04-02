01db1e307d875cb407f56cecbb56e099ca047ed4
dima <skitals.92@mail.ru>
2020-04-02 15:20:51 +0300

    The fix on the comments

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,38 +2,36 @@ package ru.netology.service;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CashbackHackServiceTest {
 
     @Test
     void remain() {
-    CashbackHackService cashbackHackService = new CashbackHackService();
-    int boundary = 1000;
-
-    int actual = cashbackHackService.remain(boundary);
-    int expected = 0;
-
-        assertEquals(expected,actual);
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int boundary = 1000;
+        int actual = cashbackHackService.remain(boundary);
+        int expected = 0;
+        assertEquals(expected, actual);
     }
+
+
     @Test
-    void remain2() {
+    void TestSeviceWhenSmallerAmount() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int boundary = 900;
-
         int actual = cashbackHackService.remain(boundary);
         int expected = 100;
-
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
+
     @Test
-    void remain3() {
+    void TestWhenAmountNotUneven() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int boundary = 854;
-
         int actual = cashbackHackService.remain(boundary);
         int expected = 146;
-
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
 }
